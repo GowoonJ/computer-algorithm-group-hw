b7bef442a15c6207d05bdd85c98900cb8f6f7ac1
GowoonJ <jgw971229@naver.com>
2020-04-21 02:26:21 +0900

    set Default Layout

--- src/CalculatorClient.java
+++ src/CalculatorClient.java
@@ -1,10 +1,38 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class CalculatorClient extends JFrame implements ActionListener, Runnable {
 
-    // JFrame 이용해서 구현하기
+    JPanel costPanel = new JPanel(new GridLayout(2,0));
+    JTextArea paymentArea = new JTextArea();
+    JTextArea totalCostArea = new JTextArea();
+
+
+    // JFrame 이용
+
+    public void start(){
+        // x 클릭 시 프로그램도 종료, Frame의 Layout : BorderLayout
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new BorderLayout());
+
+        JPanel itemPanel = new JPanel(new GridLayout(2,3));
+        itemPanel.setBackground(Color.LIGHT_GRAY);
+        this.add(itemPanel,BorderLayout.CENTER);
+
+        costPanel.setBackground(Color.WHITE);
+        this.add(itemPanel,BorderLayout.SOUTH);
+
+        this.setSize(800, 600);
+        this.setVisible(true);
+    }
+
+
+    public static void main(String[] args){
+        CalculatorClient client = new CalculatorClient();
+        client.start();
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
