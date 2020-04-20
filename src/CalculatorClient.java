b78a4e82f746e12048202ad68aa5647801110f7d
GowoonJ <jgw971229@naver.com>
2020-04-21 00:45:45 +0900

    checkout develop branch

--- src/CalculatorClient.java
+++ src/CalculatorClient.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionListener;
 
 public class CalculatorClient extends JFrame implements ActionListener, Runnable {
 
+    // JFrame 이용해서 구현하기
 
     @Override
     public void actionPerformed(ActionEvent e) {
--- src/GreedyAlgorithm.java
+++ src/GreedyAlgorithm.java
@@ -1,2 +1,4 @@
 public class GreedyAlgorithm {
+
+    // Todo 둘이 파트 나눠서 브랜치명 feature/algorithm 이런식으로 만들고 푸시할 것
 }
