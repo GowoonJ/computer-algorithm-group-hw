10c81864561decf2500cad571931e53834906bd5
GowoonJ <jgw971229@naver.com>
2020-04-21 03:08:03 +0900

    edit Calculator GUI Layout

---
+++ src/CalculatorApp.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class CalculatorApp {
+    public JPanel mainJPanel;
+    private JSpinner spinner2;
+    private JSpinner spinner3;
+    private JTextArea costArea;
+    private JTextArea totalCostArea;
+    private JButton buttonStart;
+    private JButton buttonBuy;
+    private JLabel count2Label;
+    private JLabel count3Label;
+    private JPanel item1Panel;
+    private JPanel item3Panel;
+    private JPanel item2Panel;
+    private JLabel count1Label;
+    private JPanel calculateJPanel;
+    private JLabel totalLabel;
+    private JLabel costLabel;
+    private JLabel titleLabel;
+    private JPanel titleJPanel;
+}
--- src/CalculatorClient.java
+++ src/CalculatorClient.java
@@ -9,23 +9,24 @@ public class CalculatorClient extends JFrame implements ActionListener, Runnable
     JTextArea paymentArea = new JTextArea();
     JTextArea totalCostArea = new JTextArea();
 
+    CalculatorApp calculatorApp = new CalculatorApp();
+    JPanel mainPanel = calculatorApp.mainJPanel;
+
 
     // JFrame 이용
 
     public void start(){
         // x 클릭 시 프로그램도 종료, Frame의 Layout : BorderLayout
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLayout(new BorderLayout());
 
-        JPanel itemPanel = new JPanel(new GridLayout(2,3));
-        itemPanel.setBackground(Color.LIGHT_GRAY);
-        this.add(itemPanel,BorderLayout.CENTER);
 
-        costPanel.setBackground(Color.WHITE);
-        this.add(itemPanel,BorderLayout.SOUTH);
-
-        this.setSize(800, 600);
-        this.setVisible(true);
+        JFrame frame = new JFrame("App");
+        frame.setContentPane(new CalculatorApp().mainJPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setBackground(Color.LIGHT_GRAY);
+//        mainPanel.setBackground(Color.LIGHT_GRAY);
+        frame.setSize(800, 600);
+        frame.setVisible(true);
     }
 
 
