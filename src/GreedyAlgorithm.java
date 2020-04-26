e5573d872151aec7b52df726af142fbc8288ad9a
GowoonJ <jgw971229@naver.com>
2020-04-26 17:37:38 +0900

    이전 데이터의 중복 출력 방지를 위해 arrayList clear() 추가

--- src/GreedyAlgorithm.java
+++ src/GreedyAlgorithm.java
@@ -1,39 +1,16 @@
-import java.lang.reflect.Array;
-import java.util.*;
+import java.util.ArrayList;
 
 public class GreedyAlgorithm {
-    private int item1;
-    private int item2;
-    private int item3;
     private int money; // useful money input value
 
-    private ArrayList<Integer> costList = new ArrayList<>();
-
-    private int sum = 0;
-
-    private boolean isPay = false;
+    private ArrayList<String> changeArray = new ArrayList<>();
 
     public void moneyChange() {
-        int money = 0;
-//        money=sc.nextInt(); //가지고 있는돈
+        int[] arr = {10, 50, 100, 500, 1000, 5000, 10000, 50000};//지폐종류값 할당 배열 ex)1000원 50000원 등등등
 
-        while(true) {
-            if(money<sum) {
-                System.out.println("금액 부족!");
-//                money = sc.nextInt(); //금액 부족하면 다시 입력받기
-            }
-            else{
-                money = money-sum;//거스름돈 넣어줌
-                break;
-            }
-        }
+        int cnt; //지폐,동전 개수 카운트 변수
 
-
-        if(money==0) System.out.println("거스름돈이 없습니다!");
-
-        int arr[]= {100,500,1000,5000,10000,50000};//지폐종류값 할당 배열 ex)1000원 50000원 등등등
-
-        int cnt=0; //지폐,동전 개수 카운트 변수
+        changeArray.clear();
         for(int i=arr.length-1;i>=0;i--){//큰 화폐단위부터 봐줘야 하기 때문에 i를 n-1부터 줄어들게함
             cnt=0;//한바퀴 돌고 나면 카운트 0으로 초기화 그래야 다음 화폐종류의 개수가 나온다.
             while(money-arr[i]>=0){
@@ -41,13 +18,23 @@ public class GreedyAlgorithm {
                 cnt++;
             }//거스름돈 구분하고 개수 카운트
             if(money-arr[i]<arr[i]) {
-               if(cnt>0) System.out.println(arr[i]+"원은"+cnt+"개가 필요합니다");
+               if(cnt>0) {
+                   changeArray.add(arr[i]+"원\t"+cnt+"개");
+               }
 
             }//거스름돈이 화폐종류보다 작아진다면 갯수 출력
         }
     }
-
-    public Boolean getIsPay(){
+    Boolean getIsPay(int money, int sum){
+        boolean isPay = false;
+        if(money>sum) {
+            isPay = true;
+            this.money = money-sum;
+        }
         return isPay;
+    }
+
+    ArrayList<String> getChangeCost(){
+        return changeArray;
     }
 }
